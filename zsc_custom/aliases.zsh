3baa1ac33f56e7c76afd642d209feecad6347ae9
Andrii Karbanovich <andrii.karbanovich@anvileight.com>
2022-07-19 22:52:45 +0200

    add aliases

--- zsc_custom/aliases.zsh
+++ zsc_custom/aliases.zsh
@@ -29,3 +29,14 @@ alias codeo="code --reuse-window"
 # Utils                                 #
 #########################################
 alias fixpsql="brew services stop postgresql && rm -f /opt/homebrew/var/postgres/postmaster.pid && brew services start postgresql"
+
+#########################################
+# Hide / Show files                     #
+#########################################
+alias unhidelibrary="chflags nohidden ~/Library"
+
+alias showdotfiles="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hidedotfiles="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+alias hide="chflags hidden $0"
+alias unhide="chflags nohidden $0"
